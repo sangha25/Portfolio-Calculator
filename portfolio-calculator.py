07b1a8a3d7a623fa9d48331b9e5bcbe2e22d17c5
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:44:02 +0530

    Update portfolio-calculator.py

--- portfolio-calculator.py
+++ portfolio-calculator.py
@@ -6,3 +6,9 @@ file = open('Portfolio.txt','r')
 Symbols = []
 Quantitys = []
 Buy_prices = []
+
+for line in file.readlines()[1:]:
+    line = line.split(',')
+    Symbols.append(line[0])
+    Quantitys.append(float(line[1]))
+    Buy_prices.append(float(line[2]))
