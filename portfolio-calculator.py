1ea2a09524982160208e118b9e28a37a3b6cd663
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 12:46:05 +0530

    Update portfolio-calculator.py

--- portfolio-calculator.py
+++ portfolio-calculator.py
@@ -21,3 +21,31 @@ symbols_id = [0] * len(Symbols)
 api = 'https://api.coinmarketcap.com/v2/ticker/'
 table = PrettyTable()
 table.field_names = ["Name","Quantity","Buy Price", 'Current Price ', "Profit %","Change 1h","Change 1d","Change 7d"]
+for currency in data:
+    if currency['name'] in Symbols:
+        symbols_id[Symbols.index(currency['name'])] = currency['id']
+
+def color(nr):
+    if nr > 0:
+        return ('\033[92m' + str(nr) + '\033[0m')
+    else:
+        return ('\033[91m' + str(nr) + '\033[0m')
+
+current_value = 0
+buy_value = 0
+
+for i in range(len(Symbols)):
+    temp_api = api + str(symbols_id[i])
+    data = requests.get(temp_api).json()
+    current_price = float(data['data']['quotes']['USD']['price'])
+    change_1h = float(data['data']['quotes']['USD']['percent_change_1h'])
+    change_1d = float(data['data']['quotes']['USD']['percent_change_24h'])
+    change_7d = float(data['data']['quotes']['USD']['percent_change_7d'])
+
+    profit = round(current_price/Buy_prices[i]*100-100,2)
+    buy_value += Buy_prices[i] * Quantitys[i]
+    current_value += current_price * Quantitys[i]
+    table.add_row([Symbols[i],Quantitys[i],Buy_prices[i], current_price,color(profit),
+                   color(change_1h),color(change_1d),color(change_7d)])
+
+print(table)
